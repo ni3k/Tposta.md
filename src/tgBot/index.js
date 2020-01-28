c40878c1a020dc86452f0fb560b3722993104474
nicu <nicumih7@gmail.com>
2020-01-28 17:40:08 +0200

    add abstractbot and controllerbot

--- src/tgBot/index.js
+++ src/tgBot/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-new */
 import TGBot from 'node-telegram-bot-api';
 import Track from '../../db/models/tracks';
+import BotController from './botController';
 
 require('dotenv').config();
 
@@ -20,60 +22,7 @@ bot.bulkSend = function (chatId, messages) {
 
 
 bot.init = function () {
-  this.onText(/\/start/, ({ chat: { id } }) => {
-    this.bulkSend(id, this.startMessages);
-  });
-
-  this.onText(/\/list/, async ({ chat: { id } }) => {
-    const list = await Track.find({ userId: id }, 'trackingNumber');
-
-    if (list) {
-      const oneMessage = list.map(({ trackingNumber }) => `You have registered: trackingNumber, to delete /delete_${trackingNumber}\n`).join('');
-      this.sendMessage(id, oneMessage);
-    } else {
-      this.sendMessage(id, 'You have no tracking numbers registered');
-    }
-  });
-
-  this.onText(/\/delete_(.+)/, async ({ chat: { id } }, [_, trackingNumber]) => {
-    const data = await Track.findOneAndDelete({ trackingNumber, userId: id });
-    if (data) {
-      this.sendMessage(id, `removed ${trackingNumber}`);
-    } else {
-      this.sendMessage(id, `not found ${trackingNumber}`);
-    }
-  });
-
-  this.onText(/\/getData (.+)/, async ({ chat: { id } }, match) => {
-    const trackingNumber = match[1];
-    const data = await Track.findOne({ trackingNumber, userId: id }, 'trackInfo');
-
-    if (data) {
-      const oneMessage = JSON.parse(data.trackInfo).map(({ date, event }) => `on date: ${date} with event: ${event},`).join('\n');
-
-      this.sendMessage(id, oneMessage);
-    } else {
-      this.sendMessage(id, 'No registered data for this tracking number for you');
-    }
-  });
-
-  this.onText(/\/track (.+)/, async ({ chat, from: { username } }, match) => {
-    const trackingNumber = match[1];
-
-    await Track.updateOne({ userId: chat.id, trackingNumber }, {
-      trackingNumber,
-      userId: chat.id,
-      username,
-      lastUpdated: new Date(),
-      trackInfo: null,
-      lastEvent: null,
-    },
-    {
-      upsert: true,
-    });
-
-    this.sendMessage(chat.id, `Started tracking ${trackingNumber}, will stop if there are no updates in 2 weeks`);
-  });
+  new BotController(bot, { Track });
 
   if (TEST_MODE) {
     this.on('message', (msg) => {
